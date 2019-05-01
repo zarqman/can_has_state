56348518824c9c5e8fbb2bd3d5e60df396b0b9cb
thomas morgan <tm@iprog.com>
2019-04-30 18:29:57 -0600

    fix tests for rails 5.1+

--- test/can_has_state_test.rb
+++ test/can_has_state_test.rb
@@ -24,8 +24,8 @@ class Account
 
   def fake_persist
     if valid?
-      # just reset dirty tracking and pretend all is good
-      changed_attributes.clear
+      # reset dirty tracking as if we had persisted
+      changes_applied
       true
     end
   end
